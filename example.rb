ef63720ac86d3050116314ebc4b010033ebcbef4
Spike Grobstein <spikegrobstein@mac.com>
2012-01-21 18:03:10 -0500

    fixed the example to use catshow-file

--- example.rb
+++ example.rb
@@ -3,7 +3,7 @@ $:.push File.expand_path("../lib", __FILE__)
 require 'rubygems'
 require 'pp'
 
-require 'catshow_file'
+require 'catshow-file'
 
 ##
 # Usage: ruby example.rb <path_to_tvshows_directory>
