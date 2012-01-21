4700df0c9a10e94d38d2ee703c396ce878f957d3
Spike Grobstein <spikegrobstein@mac.com>
2012-01-21 16:42:58 -0500

    got the thing actually working. w00t!

--- lib/catshow_file.rb
+++ lib/catshow_file.rb
@@ -7,7 +7,9 @@ module Catshow
   EPISODE_SUFFIXES = %w( .mkv .avi .mp4 )
 
   TVSHOW_NFO_FILENAME = "tvshow.nfo"
-  EPISODE_NFO_FILEEXTENSION = ".nfo"
-  
-  File.include CatshowFile::File
+  EPISODE_NFO_FILEEXTENSION = ".nfo"  
+end
+
+class File
+  include CatshowFile
 end
